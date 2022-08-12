38493059447b07c37dc86051e606e64129216f55
kienth-gem <61769945+kienth-gem@users.noreply.github.com>
2022-08-12 13:38:38 +0700

    add table and alias path (#4)

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PRODUCTS} from "@core/services/mock-products";
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent {}
+export class ProductComponent implements OnInit {
+  products = PRODUCTS
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
