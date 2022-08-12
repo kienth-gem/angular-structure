6da3d4238c1631f33518b6bfd67eebdf85327127
kienth <kienth@gemvietnam.com>
2022-08-12 10:38:26 +0700

    add antd lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,16 @@ import {HeaderComponent} from "./shared/components/header/header.component";
 import { AppRoutingModule } from './app-routing.module';
 import {DashboardComponent} from "./pages/dashboard/dashboard.component";
 import {ProductComponent} from "./pages/product/product.component";
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -16,9 +26,13 @@ import {ProductComponent} from "./pages/product/product.component";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NzButtonModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
